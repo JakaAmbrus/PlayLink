94469732d6b6e565dbc3595ac664aa2961edbb2b
Jaka <jakaambrus1999@gmail.com>
2023-10-23 20:50:53 +0200

    created a docker postgresql container with a table that is connected to my api

---
+++ server/Program.cs
@@ -0,0 +1,34 @@
+using server.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using server.Entities;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<DataContext>(options =>
+{
+options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+   
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();

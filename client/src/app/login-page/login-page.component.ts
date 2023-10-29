adf70b0bd4a22bdabed3959143db1c122667c88c
Jaka <jakaambrus1999@gmail.com>
2023-10-29 19:02:09 +0100

    added animations for navigating between tabs making it look very smooth

---
+++ client/src/app/login-page/login-page.component.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-page',
+  templateUrl: './login-page.component.html',
+  styleUrls: ['./login-page.component.scss'],
+})
+export class LoginPageComponent {
+  users: any;
+
+  constructor(private http: HttpClient) {}
+  ngOnInit(): void {
+    this.http.get('https://localhost:7074/api/users').subscribe({
+      next: (data) => {
+        this.users = data;
+      },
+      error: (err) => {
+        console.error('There was an error!', err);
+      },
+      complete: () => {
+        console.log('Request completed');
+      },
+    });
+  }
+}
--- server/Program.cs
+++ server/Program.cs
@@ -29,7 +29,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
-app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 app.MapControllers();
 

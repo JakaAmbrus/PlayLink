82e0d9803508b573a41658421452add82b10b58f
Jaka <jakaambrus1999@gmail.com>
2023-11-05 22:05:28 +0100

    my initial entities structure for my desired features

---
+++ server/Entities/GroupChatUser.cs
@@ -0,0 +1,11 @@
+﻿namespace server.Entities
+{
+    public class GroupChatUser
+    {
+        public int AppUserId { get; set; }
+        public AppUser AppUser { get; set; }
+
+        public int GroupChatId { get; set; }
+        public GroupChat GroupChat { get; set; }
+    }
+}
---
+++ server/Entities/GroupMessage.cs
@@ -0,0 +1,14 @@
+﻿namespace server.Entities
+{
+    public class GroupMessage
+    {
+        public int GroupMessageId { get; set; }
+        public string? Message { get; set; }
+        public DateTime MessageTime { get; set; } = DateTime.UtcNow;
+
+        public int AppUserId { get; set; }
+        public AppUser AppUser { get; set; }
+        public int GroupChatId { get; set; }
+        public GroupChat GroupChat { get; set; }     
+    }
+}

18efda6f3cb09c6426a781d3f515981a040db8f5
Jaka <jakaambrus1999@gmail.com>
2023-11-22 16:12:34 +0100

    navigation updates

--- client/src/app/header/header-dropdown/header-dropdown.component.ts
+++ client/src/app/header/header-dropdown/header-dropdown.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { AccountService } from '../../_services/account.service';
 
@@ -9,6 +9,15 @@ import { AccountService } from '../../_services/account.service';
 })
 export class HeaderDropdownComponent {
   constructor(private accountService: AccountService, private router: Router) {}
+  @Input() username: string | null = null;
+
+  navigateToEditProfile() {
+    this.router
+      .navigateByUrl('/RefreshComponent', { skipLocationChange: true })
+      .then(() => {
+        this.router.navigate(['/user', this.username, 'edit']);
+      });
+  }
 
   logout() {
     this.accountService.logout();

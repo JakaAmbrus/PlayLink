02420806773f0f6a084b2fae49e437f282ec2e78
Jaka <jakaambrus1999@gmail.com>
2023-11-28 23:56:54 +0200

    fetching the messages of user in client

--- client/src/app/_services/messages.service.ts
+++ client/src/app/_services/messages.service.ts
@@ -1,9 +1,25 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Message } from '../_models/messages';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MessagesService {
+  baseUrl = environment.apiUrl;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getUserMessages(pageNumber: number, pageSize: number, container: string) {
+    let params = new HttpParams()
+      .set('pageNumber', pageNumber.toString())
+      .set('pageSize', pageSize.toString())
+      .set('Container', container);
+
+    return this.http.get<{ messages: Message[] }>(
+      this.baseUrl + 'messages/user',
+      { observe: 'response', params }
+    );
+  }
 }
--- client/src/app/profile/message/message.component.ts
+++ client/src/app/profile/message/message.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Message } from 'src/app/_models/messages';
+import { Pagination } from 'src/app/_models/pagination';
+import { MessagesService } from 'src/app/_services/messages.service';
 
 @Component({
   selector: 'app-message',
   templateUrl: './message.component.html',
-  styleUrls: ['./message.component.scss']
+  styleUrls: ['./message.component.scss'],
 })
-export class MessageComponent {
+export class MessageComponent implements OnInit {
+  messages: Message[] | undefined;
+  pagination: Pagination | undefined;
+  container: string = 'Unread';
+  pageNumber: number = 1;
+  pageSize: number = 6;
 
+  constructor(private messagesService: MessagesService) {}
+
+  ngOnInit(): void {
+    this.loadMessages();
+  }
+
+  loadMessages(): void {
+    this.messagesService
+      .getUserMessages(this.pageNumber, this.pageSize, this.container)
+      .subscribe({
+        next: (response) => {
+          this.messages = response.body?.messages;
+          console.log(this.messages);
+        },
+        error: (error) => {
+          console.error('Error fetching messages', error);
+        },
+      });
+  }
 }

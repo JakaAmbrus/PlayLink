03fc0a18b2f462e43e1a64b5aa1d00d1386e2df0
Jaka <jakaambrus1999@gmail.com>
2023-11-29 10:55:14 +0200

    messages initial client update

--- client/src/app/messages/messages.component.ts
+++ client/src/app/messages/messages.component.ts
@@ -1,8 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Pagination } from '../_models/pagination';
+import { Message } from '../_models/messages';
+import { MessagesService } from '../_services/messages.service';
 
 @Component({
   selector: 'app-messages',
   templateUrl: './messages.component.html',
   styleUrls: ['./messages.component.scss'],
 })
-export class MessagesComponent {}
+export class MessagesComponent implements OnInit {
+  messages: Message[] | undefined;
+  pagination: Pagination | undefined;
+  container: string = 'Unread';
+  pageNumber: number = 1;
+  pageSize: number = 6;
+
+  constructor(private messagesService: MessagesService) {}
+
+  ngOnInit(): void {
+    this.loadMessages();
+  }
+
+  loadMessages(): void {
+    this.messagesService
+      .getUserMessages(this.pageNumber, this.pageSize, this.container)
+      .subscribe({
+        next: (response) => {
+          this.messages = response.body?.messages;
+          console.log(this.messages);
+        },
+        error: (error) => {
+          console.error('Error fetching messages', error);
+        },
+      });
+  }
+}

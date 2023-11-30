bba7c7601b83dd30319a376a0deaa142ca2423cd
Jaka <jakaambrus1999@gmail.com>
2023-11-30 11:25:03 +0200

    Initial signalR setup

---
+++ server/WebAPI/Hubs/PresenceHub.cs
@@ -0,0 +1,31 @@
+﻿using Infrastructure.Interfaces;
+using Microsoft.AspNetCore.SignalR;
+
+namespace WebAPI.Hubs
+{
+    public class PresenceHub : Hub
+    {
+        private readonly IAuthenticatedUserService _authenticatedUserService;
+
+        public PresenceHub(IAuthenticatedUserService authenticatedUserService)
+        {
+            _authenticatedUserService = authenticatedUserService;
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            int authUserId = _authenticatedUserService.UserId;
+
+            await Clients.Others.SendAsync("UserIsOnline", authUserId);
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            int authUserId = _authenticatedUserService.UserId;
+
+            await Clients.Others.SendAsync("UserIsOffline", authUserId);
+
+            await base.OnDisconnectedAsync(exception);
+        }
+    }
+}

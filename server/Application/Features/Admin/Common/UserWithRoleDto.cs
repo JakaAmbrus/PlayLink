2c3b9680e2fe320cdeafdc5a89cd881323ed59fc
Jaka <jakaambrus1999@gmail.com>
2023-11-30 13:47:42 +0200

    code cleanup

---
+++ server/Application/Features/Admin/Common/UserWithRoleDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Application.Features.Admin.Common
+{
+    public class UserWithRoleDto
+    {
+        public int AppUserId { get; set; }
+        public string Username { get; set; }
+        public string Gender { get; set; }
+        public string FullName { get; set; }
+        public string Role { get; set; }
+        public string ProfilePictureUrl { get; set; }
+    }
+}
--- server/WebAPI/Program.cs
+++ server/WebAPI/Program.cs
@@ -41,7 +41,6 @@ app.UseSerilogRequestLogging();
 
 ApplicationStartupExtensions.ConfigureApplication(app, app.Services);
 
-
 app.MapControllers();
 
 app.MapHub<PresenceHub>("hubs/presence");

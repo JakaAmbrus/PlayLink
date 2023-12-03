7bc4f4cf6b38a6a0ec9baeef3d12bdbe597809ca
Jaka <jakaambrus1999@gmail.com>
2023-12-03 18:41:18 +0200

    Added user avatar component for greater readability

---
+++ client/src/app/_components/user-avatar/user-avatar.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { UserAvatarComponent } from './user-avatar.component';
+
+describe('UserAvatarComponent', () => {
+  let component: UserAvatarComponent;
+  let fixture: ComponentFixture<UserAvatarComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [UserAvatarComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(UserAvatarComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});

c92de5be8a90ab1d4632ba292fd2332242ff7cdc
Jaka <jakaambrus1999@gmail.com>
2023-12-14 00:36:54 +0200

    Global styles and animations restructure

--- client/src/app/core/animations/route-animations.ts
+++ client/src/app/core/animations/route-animations.ts
@@ -57,34 +57,4 @@ export const slideInAnimation = trigger('routeAnimations', [
       query(':enter', animateChild()),
     ]
   ),
-  // transition('* => Profile', [
-  //   style({ opacity: 0, transform: 'scale(0.5)' }),
-  //   animate('500ms ease', style({ opacity: 1, transform: 'scale(1)' })),
-  // ]),
-  // Edit => Gallery, Edit => Posts, Message = > Gallery, Message => Posts, Gallery => Posts
-
-  // transition(
-  //   'Edit => Gallery, Edit => Posts, Message = > Gallery, Message => Posts, Gallery => Posts',
-  //   [
-  //     style({ position: 'relative' }),
-  //     query(':enter, :leave', [
-  //       style({
-  //         top: 0,
-  //         width: '100%',
-  //         position: 'absolute',
-  //       }),
-  //     ]),
-  //     query(':enter', [style({ transform: 'translateX(100%)' })]),
-  //     query(':leave', animateChild()),
-  //     group([
-  //       query(':leave', [
-  //         animate('280ms ease-out', style({ transform: 'translateX(-100%)' })),
-  //       ]),
-  //       query(':enter', [
-  //         animate('280ms ease-out', style({ transform: 'translateX(0%)' })),
-  //       ]),
-  //     ]),
-  //     query(':enter', animateChild()),
-  //   ]
-  // ),
 ]);

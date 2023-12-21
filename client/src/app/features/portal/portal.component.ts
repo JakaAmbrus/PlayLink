1057953ec32d99d8d719da30863f5b3a455ef005
Jaka <jakaambrus1999@gmail.com>
2023-12-21 20:51:31 +0200

    Minor style and seo improvements

--- client/src/app/features/portal/portal.component.ts
+++ client/src/app/features/portal/portal.component.ts
@@ -3,10 +3,10 @@ import { AuthFormComponent } from './components/auth-form/auth-form.component';
 import { LoginLogoComponent } from './components/login-logo/login-logo.component';
 
 @Component({
-    selector: 'app-portal',
-    templateUrl: './portal.component.html',
-    styleUrls: ['./portal.component.scss'],
-    standalone: true,
-    imports: [LoginLogoComponent, AuthFormComponent],
+  selector: 'app-portal',
+  templateUrl: './portal.component.html',
+  styleUrls: ['./portal.component.scss'],
+  standalone: true,
+  imports: [LoginLogoComponent, AuthFormComponent],
 })
 export class PortalComponent {}

51bfd98d3209a5198caf51a3bca646ece97d74ca
Jaka <jakaambrus1999@gmail.com>
2023-12-26 04:41:58 +0200

    Responsive ui changes

--- client/src/app/features/profile/profile.component.ts
+++ client/src/app/features/profile/profile.component.ts
@@ -1,14 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { ProfileUser } from '../../shared/models/users';
-import { ActivatedRoute, RouterOutlet } from '@angular/router';
+import { ActivatedRoute, Router, RouterOutlet } from '@angular/router';
 import { animate, style, transition, trigger } from '@angular/animations';
 import { slideInAnimation } from '../../core/animations/route-animations';
 import { ProfileNavigationComponent } from './components/profile-navigation/profile-navigation.component';
 import { ProfileUserCardComponent } from './components/profile-user-card/profile-user-card.component';
 import { AsyncPipe, NgIf } from '@angular/common';
 import { UserProfileService } from 'src/app/shared/services/user-profile.service';
-import { Observable } from 'rxjs';
+import { Observable, catchError, of } from 'rxjs';
 import { LocalStorageService } from 'src/app/core/services/local-storage.service';
+import { SpinnerComponent } from '../../shared/components/spinner/spinner.component';
 
 @Component({
   selector: 'app-profile',
@@ -30,15 +31,18 @@ import { LocalStorageService } from 'src/app/core/services/local-storage.service
     ProfileUserCardComponent,
     ProfileNavigationComponent,
     RouterOutlet,
+    SpinnerComponent,
   ],
 })
 export class ProfileComponent implements OnInit {
   user$?: Observable<ProfileUser>;
   isCurrentUserProfile: boolean = false;
+  loadingError: boolean = false;
 
   constructor(
     public userProfileService: UserProfileService,
     private route: ActivatedRoute,
+    private router: Router,
     private localStorageService: LocalStorageService
   ) {}
 
@@ -50,10 +54,31 @@ export class ProfileComponent implements OnInit {
 
     this.isCurrentUserProfile = this.IsCurrentUser(username);
 
-    this.user$ = this.userProfileService.getUser(username);
+    this.loadingError = false;
+    this.user$ = this.userProfileService.getUser(username).pipe(
+      catchError(() => {
+        this.loadingError = true;
+        setTimeout(() => {
+          this.router
+            .navigateByUrl('/RefreshComponent', { skipLocationChange: true })
+            .then(() => {
+              this.router.navigate(['/discover']);
+            });
+        }, 3000);
+        return of();
+      })
+    );
   }
 
   prepareRoute(outlet: any) {
+    if (outlet.isActivated) {
+      const component = outlet.activatedRoute.component;
+      if (component === ProfileNavigationComponent) {
+        return 'ProfileNavigationComponent';
+      } else if (component === ProfileUserCardComponent) {
+        return 'ProfileUserCardComponent';
+      }
+    }
     return (
       outlet && outlet.activatedRouteData && outlet.activatedRouteData.animation
     );

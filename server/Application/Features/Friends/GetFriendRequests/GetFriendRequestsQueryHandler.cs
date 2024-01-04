758ec6869a7ccdf83bfec527d3111a61fac113a1
Jaka <jakaambrus1999@gmail.com>
2024-01-04 21:54:01 +0200

    Get Friend Requests handler readability improvements

--- server/Application/Features/Friends/GetFriendRequests/GetFriendRequestsQueryHandler.cs
+++ server/Application/Features/Friends/GetFriendRequests/GetFriendRequestsQueryHandler.cs
@@ -21,51 +21,53 @@ namespace Application.Features.Friends.GetFriendRequests
 
         public async Task<GetFriendRequestsResponse> Handle(GetFriendRequestsQuery request, CancellationToken cancellationToken)
         {
-            var authorizedUser = await _context.Users.FindAsync(request.AuthUserId, cancellationToken)
+            var authorizedUser = await _context.Users.FindAsync(new object[] { request.AuthUserId, cancellationToken }, cancellationToken)
                 ?? throw new NotFoundException("Authorized user not found");
 
             string cacheKey = $"Friends:GetFriendRequests-{request.AuthUserId}";
 
-            if (!_memoryCache.TryGetValue(cacheKey, out List<FriendRequestDto> allRequests))
+            if (_memoryCache.TryGetValue(cacheKey, out List<FriendRequestDto> cachedRequests))
             {
-                var incomingRequests = await _context.FriendRequests
-                   .Include(fr => fr.Sender)
-                   .Where(fr => fr.ReceiverId == request.AuthUserId && fr.Status == FriendRequestStatus.Pending)
-                   .Select(fr => new FriendRequestDto
-                   {
-                       FriendRequestId = fr.FriendRequestId,
-                       SenderUsername = fr.Sender.UserName,
-                       SenderFullName = fr.Sender.FullName,
-                       SenderProfilePictureUrl = fr.Sender.ProfilePictureUrl,
-                       SenderGender = fr.Sender.Gender,
-                       Status = fr.Status,
-                       TimeSent = fr.TimeSent
-                   })
-                   .ToListAsync(cancellationToken);
-
-                var sentRequestResponses = await _context.FriendRequests
-                    .Where(fr => fr.SenderId == request.AuthUserId && fr.Status != FriendRequestStatus.Pending)
-                    .Select(fr => new FriendRequestDto
-                    {
-                        FriendRequestId = fr.FriendRequestId,
-                        RecipientUsername = fr.Receiver.UserName,
-                        RecipientFullName = fr.Receiver.FullName,
-                        RecipientProfilePictureUrl = fr.Receiver.ProfilePictureUrl,
-                        RecipientGender = fr.Receiver.Gender,
-                        Status = fr.Status
-                    })
-                    .ToListAsync(cancellationToken);
-
-                allRequests = incomingRequests.Concat(sentRequestResponses).ToList();
-
-                var cacheEntryOptions = new MemoryCacheEntryOptions
-                {
-                    AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(30)
-                };
-
-                _memoryCache.Set(cacheKey, allRequests, cacheEntryOptions);
+                return new GetFriendRequestsResponse { FriendRequests = cachedRequests };
             }
 
+            var incomingRequests = await _context.FriendRequests
+                .Include(fr => fr.Sender)
+                .Where(fr => fr.ReceiverId == request.AuthUserId && fr.Status == FriendRequestStatus.Pending)
+                .Select(fr => new FriendRequestDto
+                {
+                    FriendRequestId = fr.FriendRequestId,
+                    SenderUsername = fr.Sender.UserName,
+                    SenderFullName = fr.Sender.FullName,
+                    SenderProfilePictureUrl = fr.Sender.ProfilePictureUrl,
+                    SenderGender = fr.Sender.Gender,
+                    Status = fr.Status,
+                    TimeSent = fr.TimeSent
+                })
+                .ToListAsync(cancellationToken);
+
+            var sentRequestResponses = await _context.FriendRequests
+                .Where(fr => fr.SenderId == request.AuthUserId && fr.Status != FriendRequestStatus.Pending)
+                .Select(fr => new FriendRequestDto
+                {
+                    FriendRequestId = fr.FriendRequestId,
+                    RecipientUsername = fr.Receiver.UserName,
+                    RecipientFullName = fr.Receiver.FullName,
+                    RecipientProfilePictureUrl = fr.Receiver.ProfilePictureUrl,
+                    RecipientGender = fr.Receiver.Gender,
+                    Status = fr.Status
+                })
+                .ToListAsync(cancellationToken);
+
+            var allRequests = incomingRequests.Concat(sentRequestResponses).ToList();
+
+            var cacheEntryOptions = new MemoryCacheEntryOptions
+            {
+                AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(30)
+            };
+
+             _memoryCache.Set(cacheKey, allRequests, cacheEntryOptions);
+            
             return new GetFriendRequestsResponse
             {
                 FriendRequests = allRequests

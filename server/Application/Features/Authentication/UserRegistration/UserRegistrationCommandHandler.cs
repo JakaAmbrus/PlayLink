2ae3e15705bd7d3b6d9abb48994cec2295e9ec67
Jaka <jakaambrus1999@gmail.com>
2024-01-06 13:33:32 +0200

    Adding transaction to user registration incase identity add to role fails after usr creation

--- server/Application/Features/Authentication/UserRegistration/UserRegistrationCommandHandler.cs
+++ server/Application/Features/Authentication/UserRegistration/UserRegistrationCommandHandler.cs
@@ -3,7 +3,6 @@ using Application.Features.Authentication.Common;
 using Application.Interfaces;
 using Domain.Entities;
 using MediatR;
-using Microsoft.AspNetCore.Identity;
 using System.Globalization;
 
 namespace Application.Features.Authentication.UserRegistration
@@ -11,12 +10,14 @@ namespace Application.Features.Authentication.UserRegistration
     public class UserRegistrationCommandHandler : IRequestHandler<UserRegistrationCommand, UserRegistrationResponse>
     {
 
-        private readonly UserManager<AppUser> _userManager;
+        private readonly IApplicationDbContext _context;
+        private readonly IUserManager _userManager;
         private readonly ITokenService _tokenService;
         private readonly ICacheInvalidationService _cacheInvalidationService;
 
-        public UserRegistrationCommandHandler(UserManager<AppUser> userManager, ITokenService tokenService, ICacheInvalidationService cacheInvalidationService)
+        public UserRegistrationCommandHandler(IApplicationDbContext context , IUserManager userManager, ITokenService tokenService, ICacheInvalidationService cacheInvalidationService)
         {
+            _context = context;
             _userManager = userManager;
             _tokenService = tokenService;
             _cacheInvalidationService = cacheInvalidationService;
@@ -24,58 +25,77 @@ namespace Application.Features.Authentication.UserRegistration
 
         public async Task<UserRegistrationResponse> Handle(UserRegistrationCommand request, CancellationToken cancellationToken)
         {
-
-            try
+            using (var transaction = await _context.BeginTransactionAsync(cancellationToken))
             {
-                var user = new AppUser
+                try
                 {
-                    UserName = request.Username,
-                    Gender = request.Gender,
-                    FullName = FormatPropertiesToTitleCase(request.FullName),
-                    Country = FormatPropertiesToTitleCase(request.Country),
-                    DateOfBirth = request.DateOfBirth,
-                    Created = DateTime.UtcNow,
-                };
-
-                static string FormatPropertiesToTitleCase(string input)
-                {
-                    var inputInfo = CultureInfo.CurrentCulture.TextInfo;
-                    return inputInfo.ToTitleCase(input.ToLower());
-                }
-
-                var result = await _userManager.CreateAsync(user, request.Password);
-
-                if (!result.Succeeded)
-                {
-                    if (result.Errors.Any(e => e.Code == "DuplicateUserName"))
+                    var user = new AppUser
                     {
-                        throw new ConflictException("Username already exists");
+                        UserName = request.Username,
+                        Gender = request.Gender,
+                        FullName = FormatPropertiesToTitleCase(request.FullName),
+                        Country = FormatPropertiesToTitleCase(request.Country),
+                        DateOfBirth = request.DateOfBirth,
+                        Created = DateTime.UtcNow,
+                    };
+
+                    static string FormatPropertiesToTitleCase(string input)
+                    {
+                        var inputInfo = CultureInfo.CurrentCulture.TextInfo;
+                        return inputInfo.ToTitleCase(input.ToLower());
                     }
 
-                    throw new ServerErrorException(string.Join(", \n", result.Errors.Select(e => e.Description)));
-                }
+                    var result = await _userManager.CreateAsync(user, request.Password);
 
-                await _userManager.AddToRoleAsync(user, "Member");
-
-                _cacheInvalidationService.InvalidateSearchUserCache();
-                _cacheInvalidationService.InvalidateNearestBirthdayUsersCache();
-
-                return new UserRegistrationResponse
-                {
-                    User = new UserDto
+                    if (!result.Succeeded)
                     {
-                        Username = user.UserName,
-                        Token = await _tokenService.CreateToken(user),
-                        FullName = user.FullName,
-                        Gender = user.Gender,
-                        ProfilePictureUrl = user.ProfilePictureUrl
-                    }
-                };
-            }
-            catch
-            {
+                        if (result.Errors.Any(e => e.Code == "DuplicateUserName"))
+                        {
+                            throw new ConflictException("Username already exists");
+                        }
 
-                throw new ServerErrorException("Problem creating new user");
+                        throw new ServerErrorException(string.Join(", \n", result.Errors.Select(e => e.Description)));
+                    }
+
+                    result = await _userManager.AddToRoleAsync(user, "Member");
+
+                    if (!result.Succeeded)
+                    {
+                        throw new ServerErrorException("Error adding member role to user");
+                    }
+
+                    await transaction.CommitAsync(cancellationToken);
+
+                    _cacheInvalidationService.InvalidateSearchUserCache();
+                    _cacheInvalidationService.InvalidateNearestBirthdayUsersCache();
+
+                    return new UserRegistrationResponse
+                    {
+                        User = new UserDto
+                        {
+                            Username = user.UserName,
+                            Token = await _tokenService.CreateToken(user),
+                            FullName = user.FullName,
+                            Gender = user.Gender,
+                            ProfilePictureUrl = user.ProfilePictureUrl
+                        }
+                    };
+                }
+                catch (ConflictException)
+                {
+                    await transaction.RollbackAsync(cancellationToken);
+                    throw;
+                }
+                catch (ServerErrorException)
+                {
+                    await transaction.RollbackAsync(cancellationToken);
+                    throw;
+                }
+                catch (Exception)
+                {
+                    await transaction.RollbackAsync(cancellationToken);
+                    throw new ServerErrorException("Unexpected error while creating user");
+                }
             }
         }
     }

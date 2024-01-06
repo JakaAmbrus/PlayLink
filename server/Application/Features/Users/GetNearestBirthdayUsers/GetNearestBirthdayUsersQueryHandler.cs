6c908c19b35518790b0258d462b0ed9f911d0d6a
Jaka <jakaambrus1999@gmail.com>
2024-01-06 21:43:21 +0200

    Get Nearest Birthday Users handler unit testing, handler refactoring for better readability

--- server/Application/Features/Users/GetNearestBirthdayUsers/GetNearestBirthdayUsersQueryHandler.cs
+++ server/Application/Features/Users/GetNearestBirthdayUsers/GetNearestBirthdayUsersQueryHandler.cs
@@ -1,8 +1,8 @@
 ﻿using Application.Features.Users.Common;
 using Application.Interfaces;
+using Application.Utils;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
-using Application.Utils;
 using Microsoft.Extensions.Caching.Memory;
 
 namespace Application.Features.Users.GetNearestBirthdayUsers
@@ -22,51 +22,53 @@ namespace Application.Features.Users.GetNearestBirthdayUsers
         {
             string cacheKey = "Users:GetNearestBirthdayUsers";
 
-            if (!_memoryCache.TryGetValue(cacheKey, out List<UserBirthdayDto> usersWithNearestBirthday))
+            if (_memoryCache.TryGetValue(cacheKey, out List<UserBirthdayDto> cachedUsers))
             {
-                var today = DateOnly.FromDateTime(DateTime.Today);
-
-                var users = await _context.Users
-                    .Select(u => new
-                    {
-                        u.UserName,
-                        u.FullName,
-                        u.ProfilePictureUrl,
-                        u.Gender,
-                        u.DateOfBirth
-                    })
-                    .ToListAsync(cancellationToken);
-
-                usersWithNearestBirthday = users
-                    .Select(u => new
-                    {
-                        u.UserName,
-                        u.FullName,
-                        u.ProfilePictureUrl,
-                        u.Gender,
-                        u.DateOfBirth,
-                        DaysUntilBirthday = DateTimeUtils.CalculateDaysUntilBirthday(u.DateOfBirth, today)
-                    })
-                    .OrderBy(u => u.DaysUntilBirthday)
-                    .Take(3)
-                    .Select(u => new UserBirthdayDto
-                    {
-                        Username = u.UserName,
-                        FullName = u.FullName,
-                        ProfilePictureUrl = u.ProfilePictureUrl,
-                        Gender = u.Gender,
-                        DateOfBirth = u.DateOfBirth,
-                        DaysUntilBirthday = u.DaysUntilBirthday
-                    })
-                    .ToList();
-
-                var cacheEntryOptions = new MemoryCacheEntryOptions
-                {
-                    AbsoluteExpirationRelativeToNow = TimeSpan.FromDays(1)
-                };
-
-                _memoryCache.Set(cacheKey, usersWithNearestBirthday, cacheEntryOptions);
+                return new GetNearestBirthdayUsersResult { Users = cachedUsers };
             }
+
+            var today = DateOnly.FromDateTime(DateTime.Today);
+
+            var users = await _context.Users
+                .Select(u => new
+                {
+                    u.UserName,
+                    u.FullName,
+                    u.ProfilePictureUrl,
+                    u.Gender,
+                    u.DateOfBirth
+                })
+                .ToListAsync(cancellationToken);
+
+            var usersWithNearestBirthday = users
+                .Select(u => new
+                {
+                    u.UserName,
+                    u.FullName,
+                    u.ProfilePictureUrl,
+                    u.Gender,
+                    u.DateOfBirth,
+                    DaysUntilBirthday = DateTimeUtils.CalculateDaysUntilBirthday(u.DateOfBirth, today)
+                })
+                .OrderBy(u => u.DaysUntilBirthday)
+                .Take(3)
+                .Select(u => new UserBirthdayDto
+                {
+                    Username = u.UserName,
+                    FullName = u.FullName,
+                    ProfilePictureUrl = u.ProfilePictureUrl,
+                    Gender = u.Gender,
+                    DateOfBirth = u.DateOfBirth,
+                    DaysUntilBirthday = u.DaysUntilBirthday
+                })
+                .ToList();
+
+            var cacheEntryOptions = new MemoryCacheEntryOptions
+            {
+                AbsoluteExpirationRelativeToNow = TimeSpan.FromDays(1)
+            };
+
+            _memoryCache.Set(cacheKey, usersWithNearestBirthday, cacheEntryOptions);
 
             return new GetNearestBirthdayUsersResult { Users = usersWithNearestBirthday };
         }

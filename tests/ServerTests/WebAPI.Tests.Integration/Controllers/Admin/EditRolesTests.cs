95361bfc539c82592e54216bae4c7db0b7f9ee1d
Jaka <jakaambrus1999@gmail.com>
2024-01-09 18:49:40 +0200

    Admin get users with roles Integration tests

---
+++ tests/ServerTests/WebAPI.Tests.Integration/Controllers/Admin/EditRolesTests.cs
@@ -0,0 +1,23 @@
+﻿namespace WebAPI.Tests.Integration.Controllers.Admin
+{
+    public class EditRolesTests : BaseIntegrationTest
+    {
+        public EditRolesTests(IntegrationTestWebAppFactory factory) : base(factory)
+        {
+        }
+
+        private async Task InitializeTestSeedDataAsync()
+        {
+            Context.Users.Add(new AppUser { Id = 1, UserName = "tester" });
+            Context.Users.Add(new AppUser { Id = 2, UserName = "modtester4" });
+            await Context.SaveChangesAsync(CancellationToken.None);
+        }
+
+        private async Task InitializeTestAsync(List<string> roles)
+        {
+            await ResetDatabaseAsync();
+            await InitializeAuthenticatedClient(roles);
+            await InitializeTestSeedDataAsync();
+        }
+    }
+}
---
+++ tests/ServerTests/WebAPI.Tests.Integration/Models/GetUsersWithRolesResponse.cs
@@ -0,0 +1,14 @@
+﻿using Application.Features.Admin.Common;
+
+namespace WebAPI.Tests.Integration.Models
+{
+    public class GetUsersWithRolesResponse
+    {
+        public List<UserWithRolesDto> Users { get; set; }
+
+        public GetUsersWithRolesResponse()
+        {
+            Users = new List<UserWithRolesDto>();
+        }
+    }
+}

6d4540744a5115bec4b1c8b4df155bda647d11c6
Jaka <jakaambrus1999@gmail.com>
2024-01-10 18:31:31 +0200

    Seeding Guest users upon application startup and adding Admin role to the appropriate user

--- server/Infrastructure/Data/Seed.cs
+++ server/Infrastructure/Data/Seed.cs
@@ -7,6 +7,8 @@ namespace Infrastructure.Data
 {
     public static class Seed
     {
+        // Since Guest users are accessible to anyone for login, I do not mind showing their password,
+        // if these were not meant for guest login I would have used environment variables to store the password
         public static async Task SeedData(IServiceProvider serviceProvider)
         {
             var roleManager = serviceProvider.GetRequiredService<RoleManager<AppRole>>();
@@ -25,26 +27,147 @@ namespace Infrastructure.Data
                         throw new InvalidOperationException($"Error seeding '{roleName}' role");
                     }
                 }
-                /*if (roleName == "Guest")
+            }
+
+            var memberGuests = new List<AppUser>
+            {
+                new AppUser
                 {
-                    string[] usernames = { "testone", "testthree" };
-                    foreach (var username in usernames)
+                    UserName = "testone",
+                    FullName = "George Foreman",
+                    Gender = "male",
+                    Country = "United States",
+                    DateOfBirth = new DateOnly(1980, 1, 10),
+                    Created = DateTime.UtcNow,
+                },
+
+                new AppUser
+                {
+                    UserName = "testtwo",
+                    FullName = "Andreea Popescu",
+                    Gender = "female",
+                    Country = "Romania",
+                    DateOfBirth = new DateOnly(2000, 5, 15),
+                    Created = DateTime.UtcNow,
+                },
+
+                new AppUser
+                {
+                    UserName = "testthree",
+                    FullName = "Ravi Patel",
+                    Gender = "male",
+                    Country = "India",
+                    DateOfBirth = new DateOnly(1985, 11, 20),
+                    Created = DateTime.UtcNow,
+                },              
+            };
+
+            foreach (var user in memberGuests)
+            {
+                var userExist = await userManager.FindByNameAsync(user.UserName);
+                if (userExist == null)
+                {
+                    var userResult = await userManager.CreateAsync(user, "TestModPassword123");
+
+                    if (!userResult.Succeeded)
                     {
-                        var user = await userManager.FindByNameAsync(username);
-                        if (user != null)
-                        {
-                            var hasRole = await userManager.IsInRoleAsync(user, roleName);
-                            if (!hasRole)
-                            {
-                                var userRoleResult = await userManager.AddToRoleAsync(user, roleName);
-                                if (!userRoleResult.Succeeded)
-                                {
-                                    throw new InvalidOperationException($"Error assigning '{roleName}' role to user '{username}'");
-                                }
-                            }
-                        }
+                        throw new InvalidOperationException($"Error seeding '{user.UserName}' user");
                     }
-                }*/
+
+                    userResult = await userManager.AddToRoleAsync(user, "Member");
+
+                    if (!userResult.Succeeded)
+                    {
+                        throw new InvalidOperationException($"Error adding 'Member' role to '{user.UserName}' user");
+                    }
+
+                    userResult = await userManager.AddToRoleAsync(user, "Guest");
+
+                    if (!userResult.Succeeded)
+                    {
+                        throw new InvalidOperationException($"Error adding 'Guest' role to '{user.UserName}' user");
+                    }
+                }
+            }
+
+            var moderatorGuests = new List<AppUser>
+            {
+                new AppUser
+                {
+                    UserName = "modone",
+                    FullName = "Hikari Takahashi",
+                    Gender = "female",
+                    Country = "Japan",
+                    DateOfBirth = new DateOnly(1992, 2, 28),
+                    Created = DateTime.UtcNow,
+                },
+
+                new AppUser
+                {
+                    UserName = "modtwo",
+                    FullName = "Carlos Rivera",
+                    Gender = "male",
+                    Country = "Mexico",
+                    DateOfBirth = new DateOnly(1958, 8, 5),
+                    Created = DateTime.UtcNow,
+                },
+
+                new AppUser
+                {
+                    UserName = "modthree",
+                    FullName = "Anita Horva",
+                    Gender = "female",
+                    Country = "Slovenia",
+                    DateOfBirth = new DateOnly(1983, 4, 30),
+                    Created = DateTime.UtcNow,
+                }
+            };
+
+            foreach (var user in moderatorGuests)
+            {
+                var userExist = await userManager.FindByNameAsync(user.UserName);
+                if (userExist == null)
+                {
+                    var userResult = await userManager.CreateAsync(user, "TestPassword123");
+
+                    if (!userResult.Succeeded)
+                    {
+                        throw new InvalidOperationException($"Error seeding '{user.UserName}' user");
+                    }
+
+                    userResult = await userManager.AddToRoleAsync(user, "Member");
+
+                    if (!userResult.Succeeded)
+                    {
+                        throw new InvalidOperationException($"Error adding 'Member' role to '{user.UserName}' user");
+                    }
+
+                    userResult = await userManager.AddToRoleAsync(user, "Moderator");
+
+                    if (!userResult.Succeeded)
+                    {
+                        throw new InvalidOperationException($"Error adding 'Moderator' role to '{user.UserName}' user");
+                    }
+
+                    userResult = await userManager.AddToRoleAsync(user, "Guest");
+
+                    if (!userResult.Succeeded)
+                    {
+                        throw new InvalidOperationException($"Error adding 'Guest' role to '{user.UserName}' user");
+                    }
+                }
+            }
+
+            // Admin user role only reserved for the user with username "admin". if you want to test admin
+            // functionality register under username "admin" and restart the application
+            var adminUser = await userManager.FindByNameAsync("admin");
+            if (adminUser != null && !await userManager.IsInRoleAsync(adminUser, "Admin"))
+            {
+                var adminRoleResult = await userManager.AddToRoleAsync(adminUser, "Admin");
+                if (!adminRoleResult.Succeeded)
+                {
+                    throw new InvalidOperationException("Error adding 'Admin' role to 'admin' user");
+                }
             }
         }
     }
